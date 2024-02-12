360a11ce64440c526feb6ae0a3ede60fac1527f7
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-02-12 21:01:17 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,38 @@
-
+// Compare three numbers using pass by value
+#include <stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the number: ");
+  scanf("%d", &n);
+  return n;
+}
+int compare(int a, int b, int c)
+{
+  if (a >= b && a >= c)
+  {
+    return a;
+  }
+  else if (b >= c)
+  {
+    return b;
+  }
+  else
+  {
+    return c;
+  }
+}
+void output(int a, int b, int c, int largest)
+{
+  printf("The largest of %d,%d and %d is %d", a, b, c, largest);
+}
+int main()
+{
+  int a, b, c, large;
+  a = input();
+  b = input();
+  c = input();
+  large = compare(a, b, c);
+  output(a, b, c, large);
+  return 0;
+}
