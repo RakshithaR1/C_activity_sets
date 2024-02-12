7a5205dc1b61eeb272f0fec1508187af6857424c
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-02-12 14:54:11 +0530

    test

---
+++ set01/problem04.c
@@ -0,0 +1,23 @@
+//Add two numbers using call by reference
+#include<stdio.h>
+void input(int *a,int *b)
+{
+  printf("Enter the values of a and b: ");
+  scanf("%d %d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+  *sum=a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("The sum of %d and %d is %d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
+}
