ccd529adb785a52306579d87fb235be385fae337
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-02-12 22:04:47 +0530

    test

---
+++ set01/problem11.c
@@ -0,0 +1,35 @@
+// program to add two complex numbers
+#include <stdio.h>
+struct _complex
+{
+  float real;
+  float imaginary;
+};
+typedef struct _complex Complex;
+Complex input_complex()
+{
+  Complex c;
+  printf("Enter the real and imaginary part of the number: ");
+  scanf("%d %d", &c.real, &c.imaginary);
+  return c;
+}
+Complex add_complex(Complex a, Complex b)
+{
+  Complex sum;
+  sum.imaginary = a.imaginary + b.imaginary;
+  sum.real = a.real + b.real;
+  return sum;
+}
+void output(Complex a, Complex b, Complex sum)
+{
+  printf("The sum of %.2f+%.2fi and %.2f+%.2fi is %.2f+%.2fi", a.real, a.imaginary, b.real, b.imaginary, sum.real, sum.imaginary);
+}
+int main()
+{
+  Complex a, b, result;
+  a = input_complex();
+  b = input_complex();
+  result = add_complex(a, b);
+  output(a, b, result);
+  return 0;
+}
