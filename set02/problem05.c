c23d02e5dd36370886cadd988e69831f233a1560
Rakshitha <rakshithar.bsc23@rvu.edu.in>
2024-02-13 00:20:15 +0530

    test

---
+++ set02/problem05.c
@@ -0,0 +1,33 @@
+//Program to find GCD of two numbers
+#include<stdio.h>
+int input()
+{
+  int n;
+  printf("Enter the number: ");
+  scanf("%d",&n);
+  return n;
+}
+int find_gcd(int a,int b)
+{
+  int temp;
+  while(b!=0)
+  {
+    temp=b;
+    b=a%b;
+    a=temp;
+  }
+  return a;
+}
+void output(int a,int b,int gcd)
+{
+  printf("The GCD of %d and %d is %d",a,b,gcd);
+}
+int main()
+{
+  int a,b,gcd;
+  a=input();
+  b=input();
+  gcd=find_gcd(a,b);
+  output(a,b,gcd);
+  return 0;
+}
